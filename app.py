ad4a7b1d0fcb8306371bc3aefe0e549b8c48cc76
David Panofsky <dpanofsky@singleplatform.com>
2021-06-21 19:13:05 -0400

    flake8

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from infra.backstage import BackstageStack
 props = dotenv_values()
 stack_name = props.get('TAG_STACK_NAME', 'backstage')
 
-# Using a hosted dns zone requires specifying account and region 
-env =core.Environment(account=props.get('AWS_ACCOUNT'), region=props.get('AWS_REGION', 'us-east-1'))
+# Using a hosted dns zone requires specifying account and region
+env = core.Environment(account=props.get('AWS_ACCOUNT'), region=props.get('AWS_REGION', 'us-east-1'))
 
 app = core.App()
 backstage = BackstageStack(app, stack_name, props, env=env)
 
 # be nice and tag all these resources so their are attributable
-core.Tags.of(backstage).add("Stack:Name",stack_name)
-core.Tags.of(backstage).add("Stack:Author",props.get('TAG_STACK_AUTHOR', 'foo.bar@example.com'))
+core.Tags.of(backstage).add("Stack:Name", stack_name)
+core.Tags.of(backstage).add("Stack:Author", props.get('TAG_STACK_AUTHOR', 'foo.bar@example.com'))
 
 app.synth()
